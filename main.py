9a15819c7da946a512fb4b35c5134656a030da31
Jagraj Aulakh <aulakhjagraj@outlook.com>
2018-04-28 12:56:50 -0400

    Fixed Spaces

--- main.py
+++ main.py
@@ -2,18 +2,17 @@
 from pygame import * 
 init()
 screen = display.set_mode((display.Info().current_w, display.Info().current_h))
-                                 
+
 running = True
 while running:
-    for evt in event.get():  
-        if evt.type == QUIT: 
-            running = False
-        if evt.type == KEYDOWN:
-        	if evt.key == K_ESCAPE:
-        		running = False    
-    mx,my=mouse.get_pos()
-    mb=mouse.get_pressed()
-                            
-    
-    display.flip() 
+	for evt in event.get():
+		if evt.type == QUIT:
+			running = False
+		if evt.type == KEYDOWN:
+			if evt.key == K_ESCAPE:
+				running = False
+	mx,my=mouse.get_pos()
+	mb=mouse.get_pressed()
+
+	display.flip()
 quit()
