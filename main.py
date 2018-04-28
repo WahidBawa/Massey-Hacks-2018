6e5ace273f58737b7c026291986b9a89987af4aa
Yoh8000 <wahidbawa10@gmail.com>
2018-04-28 12:47:01 -0400

    screen

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # main.py
+from pygame import * 
+init()
+screen = display.set_mode((display.Info().current_w, display.Info().current_h))
+                                 
+running = True
+while running:
+    for evt in event.get():  
+        if evt.type == QUIT: 
+            running = False
+        if evt.type == KEYDOWN:
+        	if evt.key == K_ESCAPE:
+        		running = False    
+    mx,my=mouse.get_pos()
+    mb=mouse.get_pressed()
+                            
+    
+    display.flip() 
+quit()
