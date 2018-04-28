7785cc822078a94781fa726b4562ff246c854a89
Jagraj Aulakh <aulakhjagraj@outlook.com>
2018-04-28 13:39:07 -0400

    Updates

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # main.py
-from pygame import * 
+from pygame import *
 init()
-screen = display.set_mode((display.Info().current_w, display.Info().current_h))
+screen = display.set_mode((display.Info().current_w, display.Info().current_h), FULLSCREEN)
 running = True
+
+def load_images():
+	global images
+	images = {}
+	images["player1"] = image.load("Sprites/")
 while running:
 	for evt in event.get():
 		if evt.type == QUIT:
@@ -10,8 +15,8 @@ while running:
 		if evt.type == KEYDOWN:
 			if evt.key == K_ESCAPE:
 				running = False
-	mx,my=mouse.get_pos()
-	mb=mouse.get_pressed()
+	mx,my = mouse.get_pos()
+	mb = mouse.get_pressed()
 
 	display.flip()
 quit()
