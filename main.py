adfedf3ea8f46af166b36abfea188dff5993c161
Jagraj Aulakh <aulakhjagraj@outlook.com>
2018-04-28 23:32:40 -0400

    Fixed Yoh8000's shit

--- main.py
+++ main.py
@@ -21,32 +21,6 @@ running = True
 myClock = time.Clock()
 fname = load_pygame("Maps/64.tmx")
 f = font.SysFont("Times New Roman", 20)
-<<<<<<< HEAD
-def randomize():
-	vals = [1,14,96]
-	width, height = 30, 17
-	with (open("Maps/64.tmx", 'w')) as f:
-		f.write('''<?xml version="1.0" encoding="UTF-8"?>
-<map version="1.0" tiledversion="1.1.2" orientation="orthogonal" renderorder="right-down" width="30" height="17" tilewidth="64" tileheight="64" infinite="0" nextobjectid="1">
- <tileset firstgid="1" name="t1" tilewidth="64" tileheight="64" spacing="10" tilecount="540" columns="27">
-  <image source="../Sprites/Spritesheet/spritesheet_tiles.png" width="1988" height="1470"/>
- </tileset>
- <layer name="Tile Layer 1" width="30" height="17">
-  <data encoding="csv">
-''')
-		c = choice(vals)
-		grid = ""
-		for y in range(height):
-			for x in range(width):
-				grid += "%d," % c
-			grid += "\n"
-
-		grid = grid[:-2] + "\n"
-		# print(grid)
-		f.write(grid)
-
-		f.write("</data>\n </layer></map>\n")
-		f.close()
 
 def randomize():
 	global fname
@@ -238,8 +212,6 @@ while running:
 	hits = sprite.groupcollide(enemies, bullets, True, True)
 	if hits:
 		score += 1
-	if score == 1:
-		randomize()
 	all_sprites.draw(screen)
 	screen.blit(f.render(str(score), True, BLACK), (0, 0))
 	draw.rect(screen, BLACK, (98, 10, 204, 40), 4)
